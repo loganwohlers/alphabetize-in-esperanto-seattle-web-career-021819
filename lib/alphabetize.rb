55fbcda072f6744a9a8d37491d37c3034ec9090e
Logan Wohlers <lawohlers@gmail.com>
2019-02-15 23:11:20 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,9 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   arr.sort_by do |word| 
-   wordarr=word.split("")
-   wordarr.map
+     wordarr=word.split("")
+     wordarr.map do |letter| 
+        ESPERANTO_ALPHABET.index(letter)
+      end
   end
 end
