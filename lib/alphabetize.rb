ba9d21d17b4e4c878f81f102cb91c606dcc90377
Logan Wohlers <lawohlers@gmail.com>
2019-02-15 23:11:52 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |word| 
+     wordarr=word.split("")
+     wordarr.map do |letter| 
+        ESPERANTO_ALPHABET.index(letter)
+      end
+  end
 end
